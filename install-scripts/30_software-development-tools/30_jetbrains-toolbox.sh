f93bff86d1ec7f28ce8122b1be7adaa1a3f24f90
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-02-13 11:27:05 +0100

    Implement install of jetbrains toolbox

--- install-scripts/30_software-development-tools/30_jetbrains-toolbox.sh
+++ install-scripts/30_software-development-tools/30_jetbrains-toolbox.sh
@@ -2,9 +2,19 @@
 export app_name="JetBrains Toolbox"
 
 function is_app_already_installed() {
-    return 0
+    if [ -d "$USER_HOME/.local/share/JetBrains/Toolbox" ]; then
+        return 1
+    else
+        return 0
+    fi
 }
 
 function install_app() {
-    return 1; # TODO
+    cd "$TMP_DIR" || exit;
+    wget -O jetbrains-toolbox.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.22.10970.tar.gz?_ga=2.149720694.1542304937.1644746805-1728814540.1639591165&_gl=1*92zwjp*_ga*MTcyODgxNDU0MC4xNjM5NTkxMTY1*_ga_V0XZL7QHEB*MTY0NDc0NjgwNC4zLjEuMTY0NDc0NzM0Mi41OQ.."
+    extract jetbrains-toolbox.tar.gz
+    rm jetbrains-toolbox.tar.gz
+
+    mv jetbrains-toolbox* jetbrains-toolbox
+    ./jetbrains-toolbox/jetbrains-toolbox
 }
