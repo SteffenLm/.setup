34f34872f3c1cefbdea1821971e3e6fc696e26d9
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-02-13 10:56:34 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # Download scripts
 wget https://github.com/ISchwarz23/.setup/archive/refs/heads/ubuntu.zip &> /dev/null
 sudo apt-get install -y unzip &> /dev/null
-unzip ubuntu.zip -d .setup &> /dev/null
+unzip ubuntu.zip &> /dev/null
 rm ubuntu.zip
 
 # Run install-local.sh
-./.setup/install-local.sh
+./.setup-ubuntu/install-local.sh
 
 # remove "ubuntu" folder
-#rm -rf .setup
+rm -rf .setup-ubuntu
