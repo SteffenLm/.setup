3f28535d28e20a41ff043ff75a8c50259afe5b2a
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-02-13 09:29:10 +0100

    Implement install script functionality

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Download
-# https://github.com/ISchwarz23/.setup/archive/refs/heads/ubuntu.zip
+# Download scripts
+wget https://github.com/ISchwarz23/.setup/archive/refs/heads/ubuntu.zip
+sudo apt-get install -y unzip
+unzip ubuntu.zip -d .setup
+rm ubuntu.zip
 
 # Run install-local.sh
+./.setup/install-local.sh
 
 # remove "ubuntu" folder
+rm -rf .setup
