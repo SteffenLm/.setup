021f7ddec322372d96e667135e0eb3f6df2f0d68
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-02-13 19:18:22 +0100

    Implement installation of fonts

--- install-scripts/90_look-and-feel/40_fonts.sh
+++ install-scripts/90_look-and-feel/40_fonts.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 export app_name="Fonts"
 
+
+fonts=('fonts-roboto' 'fonts-firacode')
+
 function is_app_already_installed() {
-    return 0
+    for font in "${fonts[@]}"; do
+        dpkg -s "$font"
+        if [ "$?" -eq 1 ]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
 function install_app() {
-    return 1; # TODO
+    for font in "${fonts[@]}"; do
+        sudo apt install -y "$font"
+    done
 }
