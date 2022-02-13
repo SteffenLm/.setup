e469d522fff8c5b6b88e8668e52120ea3b7aacc5
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-02-13 15:48:34 +0100

    Implement installation of gnome theme

--- install-scripts/90_look-and-feel/20_gnome-themes.sh
+++ install-scripts/90_look-and-feel/20_gnome-themes.sh
@@ -2,9 +2,20 @@
 export app_name="Gnome Themes"
 
 function is_app_already_installed() {
-    return 0
+    if [ -d "$USER_HOME/.themes/WhiteSur-dark-solid" ]; then
+        return 1
+    else
+        return 0
+    fi
 }
 
 function install_app() {
-    return 1; # TODO
+    cd "$TMP_DIR" || return 1;
+
+    git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+    cd "WhiteSur-gtk-theme" || return 1;
+    ./install.sh -c dark -o solid -t default -s 280 -i ubuntu
+
+    gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-dark-solid'
+    # gsettings set org.gnome.shell.extensions.user-theme name 'WhiteSur-dark-solid'
 }
