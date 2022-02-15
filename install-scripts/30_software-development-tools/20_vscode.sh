aff7ee92429ac77755965631a21119354fce4a24
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-02-15 16:43:01 +0100

    Add markdown plugin to code installation script

--- install-scripts/30_software-development-tools/20_vscode.sh
+++ install-scripts/30_software-development-tools/20_vscode.sh
@@ -21,4 +21,5 @@ function install_app() {
     code --install-extension k--kato.intellij-idea-keybindings
     code --install-extension timonwong.shellcheck
     code --install-extension codemooseus.vscode-devtools-for-chrome
+    code --install-extension yzhang.markdown-all-in-one
 }
