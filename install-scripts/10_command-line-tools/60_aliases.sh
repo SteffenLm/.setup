f6ed182f1da11197710103bec23136312b257c7b
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-03-21 20:09:30 +0100

    Add git aliases

--- install-scripts/10_command-line-tools/60_aliases.sh
+++ install-scripts/10_command-line-tools/60_aliases.sh
@@ -11,8 +11,10 @@ function is_app_already_installed() {
 
 function install_app() {
     touch "$USER_HOME/.aliases"
-    echo 'source .aliases' >> "$USER_HOME/.zshrc"
+    echo 'source ~/.aliases' >> "$USER_HOME/.zshrc"
 
     echo 'alias glg="git lg"' >> "$USER_HOME/.aliases"
+    echo 'alias gca="git commit --amand"' >> "$USER_HOME/.aliases"
+    echo 'alias gcan="git commit --amand --no-edit"' >> "$USER_HOME/.aliases"
     return 0;
 }
