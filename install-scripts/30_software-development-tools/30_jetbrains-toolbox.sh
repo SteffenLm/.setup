c030998dc67f5c3639ea4c666fd4035ccc30b48c
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-03-31 11:02:26 +0200

    Adapt jetbrains toolbox url

--- install-scripts/30_software-development-tools/30_jetbrains-toolbox.sh
+++ install-scripts/30_software-development-tools/30_jetbrains-toolbox.sh
@@ -11,7 +11,7 @@ function is_app_already_installed() {
 
 function install_app() {
     cd "$TMP_DIR" || exit;
-    wget -O jetbrains-toolbox.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.22.10970.tar.gz?_ga=2.149720694.1542304937.1644746805-1728814540.1639591165&_gl=1*92zwjp*_ga*MTcyODgxNDU0MC4xNjM5NTkxMTY1*_ga_V0XZL7QHEB*MTY0NDc0NjgwNC4zLjEuMTY0NDc0NzM0Mi41OQ.."
+    wget -O jetbrains-toolbox.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.22.10970.tar.gz"
     extract jetbrains-toolbox.tar.gz
     rm jetbrains-toolbox.tar.gz
 
