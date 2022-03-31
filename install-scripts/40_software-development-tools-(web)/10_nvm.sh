b94e3d2cd75a2dbaed404f0d736c0c405d277b17
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-03-31 14:09:16 +0200

    Fix nvm installation

--- install-scripts/40_software-development-tools-(web)/10_nvm.sh
+++ install-scripts/40_software-development-tools-(web)/10_nvm.sh
@@ -11,6 +11,10 @@ function is_app_already_installed() {
 
 function install_app() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    
+    echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> "$USER_HOME/.bashrc"
+    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> "$USER_HOME/.bashrc"
+
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 }
