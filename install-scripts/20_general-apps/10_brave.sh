2caf9e95b035209a9788d1b9226d16b5901acb60
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-04-01 06:58:14 +0200

    Change installation of brave to use snap

--- install-scripts/20_general-apps/10_brave.sh
+++ install-scripts/20_general-apps/10_brave.sh
@@ -10,9 +10,12 @@ function is_app_already_installed() {
 }
 
 function install_app() {
-    sudo apt -y install apt-transport-https
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    sudo apt -y update
-    sudo apt -y install brave-browser
+    # sudo apt -y install apt-transport-https
+    # sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+    # echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+    # sudo apt -y update
+    # sudo apt -y install brave-browser
+
+    sudo snap install brave
+    sudo cp /var/lib/snapd/desktop/applications/brave_brave.desktop /usr/share/applications/
 }
