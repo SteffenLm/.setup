c96a1a4b0c1aa1d6bfd1892b50a80834c8829756
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-04-01 08:38:52 +0200

    Make nvm work for zsh

--- install-scripts/40_software-development-tools-(web)/10_nvm.sh
+++ install-scripts/40_software-development-tools-(web)/10_nvm.sh
@@ -12,8 +12,8 @@ function is_app_already_installed() {
 function install_app() {
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
     
-    echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> "$USER_HOME/.bashrc"
-    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> "$USER_HOME/.bashrc"
+    echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"' >> "$USER_HOME/.zshrc"
+    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> "$USER_HOME/.zshrc"
 
     export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
