6a34e57b955d05f46e281eecde4b0b68c045f323
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-04-04 14:39:55 +0200

    Update docker installation

--- install-scripts/50_software-development-tools-(devops)/10_docker.sh
+++ install-scripts/50_software-development-tools-(devops)/10_docker.sh
@@ -13,9 +13,13 @@ function install_app() {
     # prepare
     sudo apt-get update -y 
     sudo apt-get install -y ca-certificates gnupg lsb-release
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -y --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    
+    # add gpg key
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    
+    # add docker repo
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+    
     # install
     sudo apt-get update -y
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
